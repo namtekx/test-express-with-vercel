64f23945b6953181e5a7bb910817d3626b6394ec
Nam Pham <nam@tekx.com.au>
2024-07-21 21:56:16 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,63 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+import http from "http";
+import path from "path";
+import { response } from "../core/modules/response.module";
 const app = express()
 const port = process.env.PORT || 8080
+const server = http.createServer(app);
+
+app.use(
+	//@TODO: add to env
+	cors({
+		origin: "*",
+	})
+);
+
+app.use(
+	bodyParser.json({
+		limit: "500mb",
+	})
+);
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+		limit: "500mb",
+		parameterLimit: 10000000,
+	})
+);
+
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb" }));
 
 app.get('/', (_req: Request, res: Response) => {
 	return res.send('Express Typescript on Vercel')
 })
 
+app.get("/test", async (req, res) => {
+	const query = req.query
+	return response(res).success({
+	  user: {
+		email: "nam@gmail.com",
+		id: "123",
+		query
+	  }
+	})
+  });
+
 app.get('/ping', (_req: Request, res: Response) => {
 	return res.send('pong 🏓')
-})
+});
 
-app.listen(port, () => {
-	return console.log(`Server is listening on ${port}`)
-})
+(async () => {
+	console.log("running");
+	server.listen(port, () => {
+		console.log(`> web application is runnning on PORT: ${port}`, {
+			port: port,
+		});
+	});
+})();
+
