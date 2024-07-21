b2dbafcdd1854697d192a465338b4a2c01350eb8
Nam Pham <nam@tekx.com.au>
2024-07-21 21:39:33 +0700

    revert

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,16 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import express, { Request, Response } from 'express'
 
-import http from "http";
-import path from "path";
-import { AuthController } from "~@/routes/controllers";
-import { response } from "~@/modules/response.module";
+const app = express()
+const port = process.env.PORT || 8080
 
-const app = express();
-const server = http.createServer(app);
-//setup
-app.use(
-  //@TODO: add to env
-  cors({
-    origin: "*",
-  })
-);
-
-app.use(
-  bodyParser.json({
-    limit: "500mb",
-  })
-);
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: "500mb",
-    parameterLimit: 10000000,
-  })
-);
-
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ limit: "500mb" }));
-app.use(express.static(path.join(__dirname, "../../../client")));
-
-app.use((err: any, req: Request, res: Response) => {
-  return res.status(err.status || 500).json({
-    isError: true,
-    payload: err.toString(),
-  });
-});
-
-//api
-app.get("/", async (req, res) => {
-  const query = req.query
-  return response(res).success({
-    user: {
-      email: "nam@gmail.com",
-      id: "123",
-      query
-    }
-  })
-})
-app.get("/test", async (req, res) => {
-  const query = req.query
-  return response(res).success({
-    user: {
-      email: "nam@gmail.com",
-      id: "123",
-      query
-    }
-  })
+app.get('/', (_req: Request, res: Response) => {
+	return res.send('Express Typescript on Vercel')
 })
 
-app.use("/api/auth", AuthController);
+app.get('/ping', (_req: Request, res: Response) => {
+	return res.send('pong 🏓')
+})
 
-//running
-(async () => {
-  console.log("running");
-  server.listen(process.env.PORT, () => {
-    console.log(`> web application is runnning on PORT: ${process.env.PORT}`, {
-      port: process.env.PORT,
-    });
-  });
-})();
+app.listen(port, () => {
+	return console.log(`Server is listening on ${port}`)
+})
